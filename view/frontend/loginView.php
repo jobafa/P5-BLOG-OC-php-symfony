e6457c65128788034a9d502c684872741044917f
abderrahim fathi <aim.fathi@gmail.com>
2022-03-31 22:23:54 +0200

    sign in and login module branch

---
+++ view/frontend/loginView.php
@@ -0,0 +1,105 @@
+﻿<?php 
+
+// GET THE HIDEN FIELD WITH CRSF TOKEN
+$token_field = get_token_field('login');
+
+ob_start(); ?>
+
+<div class="container-fluid" id = "login">
+<div class="row justify-content-center  my-3" id=login">
+  <div class="col-lg-10 "> 
+	<div class="card shadow">
+		<div class="card-header py-2">
+			<h5 class=" text-capitalize  m-0 font-weight-bold text-info">Authentification  Utilisateur</h5>
+		</div>
+ 
+      <div class="card-body">
+
+		 <?php
+			 
+			
+			// CALL TO FUNCTION is_alertMessage() TO CHECK IF WE HAVE AN ALERT MESSAGE
+
+			$message = is_alertMessage();
+
+			if (($message) && ($message != "")){
+
+				echo $message;
+
+				unset($_SESSION['actionmessage']);
+				unset($_SESSION['alert_flag']);
+			}
+		
+			if(isset($_SESSION['errors'] )){
+				?>
+				<div class="alert  text-danger my-2 alert-dismissible fade show" role="alert">
+			  <em>
+			  <?php
+					foreach($_SESSION['errors'] as $key=>$value){
+
+						echo $value.'<BR>';
+					}
+				?>
+			  </em>
+			  <button type="button" class="btn-close justify-content-end" data-bs-dismiss="alert" aria-label="Close"></button>
+			</div>
+			<?php	
+			}
+			?>
+
+        <form action="index.php?action=verifylogin" method="post">
+          <div class="form-group">
+            <label for="email">identifiant</label>
+            <input type="email" class="form-control" type="text" placeholder="Entrer votre identifiant" name="email" required>
+			<!-- <?php
+				if(isset($emailErr) && ($emailErr != '')) {
+			?>
+			<span class = "text-danger">* <?php echo $emailErr;?></span>
+			<?php
+				}
+			?> -->
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" type="password" placeholder="Mot de passe" name="password" required>
+			<!-- <?php
+				if(isset($passordlErr) && ($passordlErr != '')) {
+			?>
+			<span class = "danger">* <?php echo $passordlErr;?></span>
+			<?php
+				}
+			?> -->
+			
+          </div>
+		  <?php
+
+
+										echo $token_field;
+							?>
+          <button type="submit" class="btn btn-primary my-3">Se connecter</button>
+		  <div class="content-fluid alert-info  py-1 px-1 ">
+		  <p><small><!-- <a class="text-secondary mx-2 " href="index.php?action=passresetrequest"> --><a class="text-secondary mx-2 " href="passresetrequest.html">J'ai oubli&eacute; mon Mot de Passe</a>Vous n'avez pas encore de compte <!-- <a class=" text-secondary mx-2" href="index.php?action=signinview"> --><a class=" text-secondary mx-2" href="signinview.html#inscription">Inscrivez Vous</a></small></p>
+
+		   </div>
+
+
+        </form>
+	<!-- 	<div class="row  col-6 py-2 px-2 justify-content-right"> -->
+<!----> </div> 
+      </div>
+    </div>
+  </div>
+  </div>
+ <!--  <div class="content-fluid alert-info my-3">
+<div class="row col-2 py-2 px-2 justify-content-right">
+<a class="btn btn-primary " href="index.php?action=loginview">Se Connecter</a>
+</div>
+<div class="row  col-6 py-2 px-2 justify-content-right">
+<em>Vous n'avez pas encore de compte <a class=" text-secondary mx-2" href="index.php?action=signinview">Inscrivez Vous</a></em>
+</div>
+</div> -->
+
+
+<?php $content = ob_get_clean(); 
+require('template.php');
+?>
---
+++ view/frontend/signinView.php
@@ -0,0 +1,85 @@
+﻿<?php
+
+// GET THE HIDEN FIELD WITH CRSF TOKEN
+$token_field = get_token_field('newuser');
+
+ob_start(); ?>
+
+<!-- Begin Page Content -->
+
+<div class="container-fluid" id = "inscription">
+ <div class="card shadow mb-4">
+<div class="card-header py-3">
+	<h3 class="m-0 font-weight-bold text-info">Inscription Utilisateur</h3>
+</div>
+
+<?php
+
+// CALL TO FUNCTION is_alertMessage() TO CHECK IF WE HAVE AN ALERT MESSAGE
+
+$message = is_alertMessage();
+
+if (($message) && ($message != "")){
+
+	echo $message;
+
+	unset($_SESSION['actionmessage']);
+	unset($_SESSION['alert_flag']);
+}
+//}
+?>
+
+
+<div class="container">
+    <div class="card card-login mx-auto my-3 px-0">
+      <div class="card-body">
+			<?php
+					if(isset($_SESSION['errors'] )){
+				?>
+						<div class="alert  alert-danger my-2 alert-dismissible fade show" role="alert">
+						  <em>
+						  <?php
+								foreach($_SESSION['errors'] as $key=>$value){
+
+									echo $value.'<BR>';
+								}
+							?>
+						  </em>
+					  <button type="button" class="btn-close justify-content-end" data-bs-dismiss="alert" aria-label="Close"></button>
+					</div>
+			<?php	
+			}
+			?>
+			<form enctype="multipart/form-data" action="index.php?action=usersignin" method="post">
+				<div class="form-group">
+					<label for="pseudo" class="form-label"><i class="fas fa-user mx-1"></i>Pseudo</label><br />
+					<input type="text"  class="form-control" id="pseudo" name="pseudo" value="" required />
+				</div>
+				<div class="form-group" class="form-label">
+				<label for="email"><i class="fas fa-envelope mx-1"></i>Email</label><br />
+					<input type="text" class="form-control" id="email" name="email" value="" required />
+				</div>
+				<div class="form-group" class="form-label">
+				<label for="password"><i class="fas fa-lock mx-1"></i>Mot de Passe</label><br />
+					<input type="password" class="form-control" id="password" name="password" value=""  required/>
+				</div >
+				<div class="form-group">
+					<label for="photo" class="form-label">Photo ( Avatar )</label>
+					<input type="file" class="form-control py-1" id="photo" name="photo" />
+				</div>
+				<div class="g-recaptcha" data-sitekey="6LcRt9UeAAAAANoCcOoFihVp2eShv5YpYQia9Aw1"></div>
+				<?php
+						echo $token_field;
+				?>
+				<div class="form-group my-3">
+					<input   type="submit" class="btn btn-info" />
+				</div>
+			</form>
+</div>
+    </div>
+  </div>
+</div>
+</div>
+<?php $content = ob_get_clean(); 
+require('template.php');
+?>
