98792cb9a802a030b1e819b23b19729dc0a425fe
abderrahim fathi <34837561+jobafa@users.noreply.github.com>
2022-05-29 18:27:10 +0200

    Update PostManager.php
    update

--- model/PostManager.php
+++ model/PostManager.php
@@ -1,30 +1,32 @@
 <?php
 
-namespace OC\PhpSymfony\Blog\Model;
+namespace Models;
 
 
-require_once("model/Manager.php");
+require_once('Models/Model.php');
 
-class PostManager extends Manager
+class PostManager extends Model
 {
+		
 		private $post_userid ;
 		private $post_title;
 		private $post_lede;
 		private $post_content ;
 		private $is_enabled ;
 
+		
 
 	/**
 	 * Get total of Posts for Pagination
 	 * @param   $ispublished = post is or not published
 	 */
 
-	  public function gettotalPosts( $ispublished = '1')
+	public function gettotalPosts( $ispublished)
     {
-			$db = $this->dbConnect();
+			
 			try
 				{	
-					$sql = $db->query('SELECT count(id) AS id FROM posts WHERE is_published = "'.$ispublished.'"')->fetchAll();
+					$sql = $this->db->query('SELECT count(id) AS id FROM posts WHERE is_published = "'.$ispublished.'"')->fetchAll();
 					$totalRecrods = $sql[0]['id'];
 
 					return $totalRecrods;
@@ -32,7 +34,7 @@ class PostManager extends Manager
 				}
 			catch (Exception $e)
 				{
-					echo 'Connexion échouée : ' . $e->getMessage();
+					echo 'Connexion Ã©chouÃ©e : ' . $e->getMessage();
 				}					
 					
 	}									
@@ -42,9 +44,9 @@ class PostManager extends Manager
 	 * @param   $post_userid,  $ispublished = post is or not published
 	 */
 
-    public function getPosts($post_userid = null, $from, $is_published = null, $paginationStart, $limit)
+    public function getAll($post_userid = null, $from, $is_published = null, $paginationStart, $limit)
     {
-        $db = $this->dbConnect();
+        
 		try
 		{
 			$req =('
@@ -55,26 +57,26 @@ class PostManager extends Manager
 						if ( isset($post_userid) && ($post_userid > 0) && ( ! isset  ($from))) {
 	
 							$req.= ' WHERE user_id = "'.$post_userid.'" ORDER BY creation_date DESC ';
-							$result = $db->query($req);
+							$result = $this->db->query($req);
 							
 						}elseif( isset($is_published) && ($is_published != null)){
 
 							$req.= ' WHERE is_published = '.$is_published.' ORDER BY update_date DESC LIMIT '.$paginationStart.', '.$limit;
 							
-							$result = $db->query($req);
+							$result = $this->db->query($req);
 						
 
 						}
 						else{
 							$req.= ' ORDER BY update_date DESC ';
-							$result = $db->query($req);
+							$result = $this->db->query($req);
 							
 							
 						}
 		}
 		catch (Exception $e)
 		{
-			echo 'Connexion échouée : ' . $e->getMessage();
+			echo 'Connexion Ã©chouÃ©e : ' . $e->getMessage();
 		}					
 			
 		return $result;			
@@ -86,17 +88,16 @@ class PostManager extends Manager
 	 * @params  string $postid and $ispublished = post is or not published
 	 */
 
-  public function getPost($postId, $is_published)
+  	public function get($postId, $is_published)
     {
-        $db = $this->dbConnect();
-
+        
 		 
 		if($is_published == '1'){			
 
-			$req = $db->prepare('SELECT posts.id, title,lede, author, image, content, is_published, photo, DATE_FORMAT(posts.update_date, \'%d/%m/%Y &agrave; %Hh%imin%ss\') AS update_date_fr FROM posts  JOIN user ON posts.user_id = user.id WHERE posts.id = ? AND posts.is_published = ?');
+			$req = $this->db->prepare('SELECT posts.id, title,lede, author, image, content, is_published, photo, DATE_FORMAT(posts.update_date, \'%d/%m/%Y &agrave; %Hh%imin%ss\') AS update_date_fr FROM posts  JOIN user ON posts.user_id = user.id WHERE posts.id = ? AND posts.is_published = ?');
 			$req->execute(array($postId, $is_published));
 		}else{
-			$req = $db->prepare('SELECT id, title,lede, author, image, content, is_published, DATE_FORMAT(creation_date, \'%d/%m/%Y &agrave; %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ? ');
+			$req = $this->db->prepare('SELECT id, title,lede, author, image, content, is_published, DATE_FORMAT(creation_date, \'%d/%m/%Y &agrave; %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ? ');
 			$req->execute(array($postId));
 		}
         
@@ -105,6 +106,8 @@ class PostManager extends Manager
         return $post;
     }
 
+	
+
 		/**
 	 * Add Post
 	 * @params  post form data
@@ -112,21 +115,21 @@ class PostManager extends Manager
 
 	public function addPost( $post_userid,$post_title, $post_lede,$post_author,$post_content, $pimage_name,$is_published) 
 		{
-			$db = $this->dbConnect();
+			
        
            
-			$req = $db->prepare('INSERT INTO posts(user_id, title, lede, author, content,image, is_published, creation_date, update_date) VALUES( ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())');
+			$req = $this->db->prepare('INSERT INTO posts(user_id, title, lede, author, content,image, is_published, creation_date, update_date) VALUES( ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())');
 			$resultat = $req->execute(array($post_userid,$post_title,$post_lede,$post_author,$post_content, $pimage_name,$is_published));
 							
 			return $resultat;
-				//}
+				
             
         }
 
 
 		 public function updatePost($idpost, $data, $postimage = '') {
 	
-			$db = $this->dbConnect();
+			
 
 			try
 		{
@@ -142,7 +145,7 @@ class PostManager extends Manager
 			$sql.= ' WHERE id = :id'	;
 			
 
-            $req = $db->prepare($sql);
+            $req = $this->db->prepare($sql);
 			
 			if($postimage == '')	{
 				
@@ -166,7 +169,7 @@ class PostManager extends Manager
 		}
 			catch (Exception $e)
 		{
-			echo 'Connexion échouée : ' . $e->getMessage();
+			echo 'Connexion Ã©chouÃ©e : ' . $e->getMessage();
 		}	
 			return $resultat;
         }
@@ -179,18 +182,18 @@ class PostManager extends Manager
 
 	public function publishPost($id, $ispublished)
 	{
-		$db = $this->dbConnect();
+		
 	 try
 	{
 		
 		$sql = 'UPDATE posts SET is_published = "'.$ispublished.'"  WHERE id = '.$id;
-		$resultat = $db->query($sql);
+		$resultat = $this->db->query($sql);
 		
 		
 	}
 	catch (Exception $e)
 	{
-		echo 'Connexion échouée : ' . $e->getMessage();
+		echo 'Connexion Ã©chouÃ©e : ' . $e->getMessage();
 	}	
 		return $resultat;
 	}
@@ -200,23 +203,23 @@ class PostManager extends Manager
 	 * @param  string $userid and $postid
 	 */
 
-		public function deletePost($postId, $userid) {
+	public function deletePost($postId, $userid) {
 
-            $db = $this->dbConnect();
+		
+		
+		$query = 'DELETE FROM posts';
+		if ($postId != null) {
+			$query.= ' WHERE posts.id = ? ';
+			$deleteposts = $this->db->prepare($query);
+			$resultat = $deleteposts->execute(array($idpost));
 			
-			$query = 'DELETE FROM posts';
-			if ($postId != null) {
-				$query.= ' WHERE posts.id = ? ';
-				$deleteposts = $db->prepare($query);
-				$resultat = $deleteposts->execute(array($idpost));
-				
-			}elseif ($userid != null) {
-				$query.= '  WHERE posts.user_id = ? ';
-				$deleteposts = $db->prepare($query);
-				$resultat = $deleteposts->execute(array($userid));
-			}
-			
-            return $resultat;
-        }
+		}elseif ($userid != null) {
+			$query.= '  WHERE posts.user_id = ? ';
+			$deleteposts = $this->db->prepare($query);
+			$resultat = $deleteposts->execute(array($userid));
+		}
+		
+		return $resultat;
+	}
 
 }
