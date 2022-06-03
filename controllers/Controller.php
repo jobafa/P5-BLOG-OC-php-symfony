fc3853d59e73ddc30343ab0cc39b7e3d1103f3a7
abderrahim fathi <aim.fathi@gmail.com>
2022-06-03 19:39:10 +0200

    code checking errors fixing

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -1,15 +1,20 @@
 <?php
 namespace Controllers;
+use Inc\MessageDisplay;
+//use Inc\FileUpload;
 
 abstract class Controller
 {
 	 
 	 protected $model;
-	 protected $modelName ;
+	 protected $modelName;
+    protected $messagedisplay;
 
      public function __construct(){
 
         $this->model = new $this->modelName();
+        $this->$messagedisplay = new \Inc\MessageDisplay();
+        //$this->$fileupload = new \Inc\FileUpload();
 
      }
 }
